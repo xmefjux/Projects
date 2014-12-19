943931e9d58c63d13701432797db39231242c009
Mateusz Ciesielski <ciesielski.mateusz91@gmail.com>
2014-12-19 18:01:53 +0100

    Ćwiczenia z deklaracji.

--- Stroustrup/Source Files/File.cpp
+++ Stroustrup/Source Files/File.cpp
@@ -14,7 +14,7 @@
 #include <algorithm>
 using namespace std;
 //******************************************************************************************************
-/*template <class C, class T> int count (const C& v, T val)
+template <class C, class T> int count (const C& v, T val)
 {
 	typename C::const_iterator i = find(v.begin(), v.end(), val);
 	int n = 0;
@@ -24,7 +24,7 @@ using namespace std;
 		i = find(i, v.end(), val);
 	}
 	return n;
-}*/
+}
 //******************************************************************************************************
 /*
  * Często mamy do czynienia z kolekcjami wskaźników i chcielibyśmy wywołać metodę wskazywanego obiektu, a nie funkcję globalną.
@@ -48,6 +48,25 @@ using namespace std;
  * Mechanizm mem_fun() jest ważny, gdyż pozwala na stosowanie standardowych algorytmów dla kolekcji obiektów polimorficznych.
  */
 //******************************************************************************************************
+// ćw. [2] napisz deklaracje do definicji i definicje do deklaracji
+/*char ch; // char ch = 0;
+string s; // string s = 0;
+int licznik = 1; // int licznik;
+const double pi = 3.1414926535897932385; // do const musi być definicja, bo trzeba przypisać w momencie deklaracji i tylko wtedy
+extern int numer_bledu; // w innym pliku int numer_bledu = 0;
+
+char* imie = "Njal"; // char* imie; - nie jest zbyt rozsądne nie nadawać wskaźnikowi wartości w momencie deklaracji
+char* sezon[] = { "wiosna", "lato", "jesien", "zima" }; // char* sezon[];
+
+struct Data { int d, m, r; }; // Struct Data;
+int dzien(Data *p) { return p->d; } // int dzien(Data *p);
+double sqrt(double); // double sqrt(double x) { return 0.0 }
+template<class T> T abs(T a) { return a<0 ? -a : a;	} // template<class T> T abs(T);
+
+struct Uzytkownik; // struct Uzytkownik { };
+typedef complex<short> Punkt;
+enum piwo { Carlsberg, Tuborg, Thor };
+namespace NS { int a; }*/
 //******************************************************************************************************
 //******************************************************************************************************
 //******************************************************************************************************
