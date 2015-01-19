dfeac2de82a85d0682925bf66e47840ad2ecd8eb
Mateusz Ciesielski <ciesielski.mateusz91@gmail.com>
2015-01-20 00:59:21 +0100

    Działająca eliminacja Gaussa-Jordana.

--- Beny - Gauss/Main.cpp
+++ Beny - Gauss/Main.cpp
@@ -9,81 +9,65 @@
 #include <stdlib.h>
 using namespace std;
 //************************************************************************************************************
+void solution(int a[][20], int var);
+//************************************************************************************************************
 int main()
 {
-/*
-//------------------------------------------------------------------------------------------------------------
-	// rozwiązywanie układu równań metodą Gaussa
-	// jedynie układy oznaczone, ilość niewiadomych = ilość równań
-	static int N = 3;						// ilość niewiadomych
-	double *macierz_wspolcz;			// wskaźnik na obszar z macierzą współczynników
-	double *macierz_wyr_woln;			// macierz wyrazów wolnych
-	double *result;
-	static double ratio;
-//------------------------------------------------------------------------------------------------------------
-	// pobieranie danych z pliku
-	ifstream myfile ("dane.txt");
-	if(myfile.is_open())
-	{
-		myfile >> N;
-		macierz_wspolcz = new double[(N*N)];
-		macierz_wyr_woln = new double[N];
-		result = new double[N];
-		for(int i = 0; i < (N*N); i++)
-		{
-			myfile >> macierz_wspolcz[i];
-		}
-		for(int i = 0; i < N; i++)
-		{
-			myfile >> macierz_wyr_woln[i];
-		}
-	}
-	myfile.close();
-*/
+	// rozwiązywanie układu równań metodą eliminacji Gaussa-Jordana
+    int a[20][20];
+    int var;
+    int i;
+    int j;
+    char znaki[] = { 'x', 'y', 'z', 'u', 'w' };
 
-	int i,j,k,n;
-	    float A[20][20],c,x[10],sum=0.0;
-	    printf("\nEnter the order of matrix: ");
-	    scanf("%d",&n);
-	    printf("\nEnter the elements of augmented matrix row-wise:\n\n");
-	    for(i=1; i<=n; i++)
-	    {
-	        for(j=1; j<=(n+1); j++)
-	        {
-	            printf("A[%d][%d] : ", i,j);
-	            scanf("%f",&A[i][j]);
-	        }
-	    }
-	    for(j=1; j<=n; j++) /* loop for the generation of upper triangular matrix*/
-	    {
-	        for(i=1; i<=n; i++)
-	        {
-	            if(i>j)
-	            {
-	                A[j][j] == 0 ? c = 0 : c = A[i][j] / A[j][j];
-	                for(k=1; k<=n+1; k++)
-	                {
-	                    A[i][k] = A[i][k] - (c * A[j][k]);
-	                }
-	            }
-	        }
-	    }
-	    A[n][n] == 0 ? x[n] = 0 : x[n] = A[n][n+1] / A[n][n];
-	    /* this loop is for backward substitution*/
-	    for(i=n-1; i>=1; i--)
-	    {
-	        sum=0;
-	        for(j=i+1; j<=n; j++)
-	        {
-	            sum = sum + A[i][j] * x[j];
-	        }
-	        A[i][i] == 0 ? x[i] = 0 : x[i] = (A[i][n+1] - sum) / A[i][i];
-	    }
-	    printf("\nThe solution is: \n");
-	    for(i=1; i<=n; i++)
-	    {
-	        printf("\nx%d=%f\t",i,x[i]); /* x1, x2, x3 are the required solutions*/
-	    }
-	    return(0);
+    cout << "Wpisz liczbe zmiennych: ";
+    cin >> var;
+
+    for (i = 0; i < var; i++)
+    {
+        cout << "Podaj rownanie " << i+1 << endl;
+        for (j = 0; j < var; j++)
+        {
+            cout << "Podaj wpolczynnik " << znaki[j] << ": ";
+            cin >> a[i][j];
+        }
+        cout << "Podaj stala: ";
+        cin >> a[i][var];
+    }
+
+    solution(a, var);
+
+    return 0;
+}
+//************************************************************************************************************
+void solution(int a[][20], int var)
+{
+    int k;
+    int i;
+    int l;
+    int j;
+    char znaki[] = { 'x', 'y', 'z', 'u', 'w' };
+
+    for (k = 0; k < var; k++)
+    {
+        for (i = 0; i <= var; i++)
+        {
+            l = a[i][k];
+            for (j = 0; j <= var; j++)
+            {
+            	if(i != k) a[i][j] = (a[k][k] * a[i][j]) - (l * a[k][j]);
+            }
+        }
+    }
+
+    cout << "Rozwiazania:" << endl;
+
+    for (i = 0; i < var; i++)
+    {
+        a[i][i] == 0 ?
+        		cout << "Wartosc " << znaki[i] << " = 0" << endl :
+				cout << "Wartosc " << znaki[i] << " = " << static_cast<double>(a[i][var]) / static_cast<double>(a[i][i]) << endl;
+    }
+
 }
 //************************************************************************************************************
