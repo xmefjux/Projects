33399279c5352e66dcaf8d85dd4716f06dc030aa
Mateusz Ciesielski <ciesielski.mateusz91@gmail.com>
2015-01-27 14:33:57 +0100

    Beny - Gauss: cd...

--- Beny - Gauss/Main.cpp
+++ Beny - Gauss/Main.cpp
@@ -9,17 +9,18 @@
 #include <stdlib.h>
 using namespace std;
 //************************************************************************************************************
-void solution(int a[][20], int var);
+void solution(int a[][6], int var);
 //************************************************************************************************************
 int main()
 {
 	// rozwiązywanie układu równań metodą eliminacji Gaussa-Jordana
-    int a[20][20];
+    int a[6][6];
     int var;
     int i;
     int j;
     char znaki[] = { 'x', 'y', 'z', 'u', 'w' };
 
+    cout << "Podaj rownania, wspolczynniki przy zmiennych musza byc rozne od zera." << endl;
     cout << "Wpisz liczbe zmiennych: ";
     cin >> var;
 
@@ -35,12 +36,32 @@ int main()
         cin >> a[i][var];
     }
 
+    cout << endl;
+
+    for (i = 0; i < var; i++)
+    {
+        for (j = 0; j < var; j++)
+        {
+        	if(a[i][j] != 1 && a[i][j] != (-1))
+        		a[i][j] > 0 ? cout << a[i][j] << znaki[j] : cout << a[i][j]*(-1) << znaki[j];
+        	else
+        		cout << znaki[j];
+            if(j < var-1)
+            	a[i][j+1] > 0 ? cout << " + " : cout << " - ";
+        }
+        cout << " = " << a[i][var] << endl;
+    }
+
+    cout << endl;
+
+    cout << "Program bedzie dazyl do utworzenia macierzy jednostkowej na podstawie wspolczynnikow oraz wyrazow wolnych." << endl;
+
     solution(a, var);
 
     return 0;
 }
 //************************************************************************************************************
-void solution(int a[][20], int var)
+void solution(int a[][6], int var)
 {
     int k;
     int i;
@@ -53,10 +74,13 @@ void solution(int a[][20], int var)
         for (i = 0; i <= var; i++)
         {
             l = a[i][k];
+            cout << l << endl;
             for (j = 0; j <= var; j++)
             {
             	if(i != k) a[i][j] = (a[k][k] * a[i][j]) - (l * a[k][j]);
+            	cout << a[i][j];
             }
+            cout << endl;
         }
     }
 
@@ -65,8 +89,8 @@ void solution(int a[][20], int var)
     for (i = 0; i < var; i++)
     {
         a[i][i] == 0 ?
-        		cout << "Wartosc " << znaki[i] << " = 0" << endl :
-				cout << "Wartosc " << znaki[i] << " = " << static_cast<double>(a[i][var]) / static_cast<double>(a[i][i]) << endl;
+        		cout << znaki[i] << " = 0" << endl :
+				cout << znaki[i] << " = " << static_cast<double>(a[i][var]) / static_cast<double>(a[i][i]) << endl;
     }
 
 }
